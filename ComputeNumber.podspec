48875ff08c65c980001d1380e2d08f7e1c4e8774
whitesky2 <dkosooo91@gmail.com>
2021-08-23 21:24:58 +0900

    21.08.23

--- ComputeNumber.podspec
+++ ComputeNumber.podspec
@@ -8,8 +8,8 @@
 
 Pod::Spec.new do |s|
   s.name             = 'ComputeNumber'
-  s.version          = '0.1.0'
-  s.summary          = 'A short description of ComputeNumber.'
+  s.version          = '0.1.1'
+  s.summary          = 'This library compute numbers.'
 
 # This description is used to generate tags and improve search results.
 #   * Think: What does it do? Why did you write it? What is the focus?
---
+++ ComputeNumber/Classes/Compute.swift
@@ -0,0 +1,17 @@
+public class Compute {
+    public static func add(num1: Int, num2: Int) -> Int {
+        return num1 + num2
+    }
+    
+    public static func substract(num1: Int, num2: Int) -> Int {
+        return num1 - num2
+    }
+    
+    public static func multiply(num1: Int, num2: Int) -> Int {
+        return num1 * num2
+    }
+    
+    public static func divide(num1: Int, num2: Int) -> Double {
+        return Double(num1 / num2)
+    }
+}
